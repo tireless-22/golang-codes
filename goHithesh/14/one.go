2a6dbca8927087a5c8944c73a71e9dc842c3fa24
karth <karthiknaveen22022002@gmail.com>
2022-09-12 23:53:35 +0530

    added slices

---
+++ goHithesh/14/one.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// slices are most used thing in golang
+// slices will depend on the arrays only
+// but they functionality more than the arrays
+
+
+// SLICE VS ARRAY
+//The basic difference between a slice and an array 
+// is that a slice is a reference to a contiguous segment of an array.
+// Unlike an array, which is a value-type, slice is a reference type. 
+// A slice can be a complete array or a part of an array, 
+// indicated by the start and end index. 
+
+
+func main(){
+	fmt.Println("hello")
+	var fruits=[] string{"hello"}
+	// FOR slice we did not declare anyslize
+	fmt.Println(fruits)
+	fmt.Printf(" is %T",fruits)
+
+	fruits=append(fruitsjl'"hi")
+
+
+
+
+}
+
+
+
+
+
+
+
